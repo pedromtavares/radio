b197512996f5234b99f503c60a916191a5cb7841
Pedro Mateus Tavares <pedro@pedromtavares.com>
2011-08-14 04:27:51 -0300

    client code fixes

--- public/js/application.js
+++ public/js/application.js
@@ -1,56 +1,68 @@
 $(function(){
   
+  function startPlayer(){
+    $("#jplayer").jPlayer({
+      ready: function () {
+        $(this).jPlayer("setMedia", {
+          mp3: "/stream.mp3",
+          oga: "/stream.ogg"
+        }).jPlayer("play");
+      },
+      swfPath: "/public",
+      supplied: "mp3, oga"
+    });
+  }
+  
+  function stopPlayer(){
+    $("#jplayer").jPlayer("clearMedia");
+  }
+  
+  function goOnline(dj){
+    $('#dj').html(dj);
+    startPlayer();
+  }
+  
   function goOffline(){
     $('#current_dj').hide();
     $('#current_track').html("Rádio está offline. Tente recarregar o navegador ou tente mais tarde.");
+    stopPlayer();
+  }
+  
+  function nextTrack(track){
+    // Don't show the next track immediately since the stream delay is about 15 seconds, so we don't want to spoil out 
+    // what the next track is gonna be 15 seconds before it actually starts. It's ok to show it immediately if 
+    // there was nothing playing (or if you just connected to the stream).
+    var current = $('#track').html();
+    var time = current == "" ? 1 : 20;
+    setTimeout(function() {
+      $('#track').html(track);
+    }, time * 1000)
+    console.log(track);
   }
 
-  //var url = "http://localhost:9000/faye"
-  var url = "http://radio.pedromtavares.com/faye"
+  var url = "http://localhost:9000/faye"
+  //var url = "http://radio.pedromtavares.com/faye"
     
   var client = new Faye.Client(url, {
     timeout: 120
   });
-  
-  var currentDJ;
-  
+    
   $.get('/dj', function(data) {
     if (data == ''){
       goOffline();
     }else{
-      currentDJ = data;
-      $('#dj').html(data);
+      goOnline(data);
     }
   });
   
   client.subscribe('/track', function (message) {
     var track = message.track;
-    var current = $('#track').html();
-    var time = 0;
-    console.log(track);
     if (track == 'offline'){
       goOffline();
     }else{
-      // Don't show the next track immediately since the stream delay is about 15 seconds, so we don't want to spoil out 
-      // what the next track is gonna be 15 seconds before it actually starts. It's ok to show it immediately if 
-      // there was nothing playing (or if you just connected to the stream).
-      time = current == "" ? 1 : 20;
-      setTimeout(function() {
-        $('#track').html(track);
-      }, time * 1000)
+      nextTrack(track);
     }
   });
   
-  $("#jplayer").jPlayer({
-    ready: function () {
-      if (currentDJ){
-        $(this).jPlayer("setMedia", {
-          mp3: "/stream.mp3",
-          oga: "/stream.ogg"
-        }).jPlayer("play");
-      }
-    },
-    swfPath: "/public",
-    supplied: "mp3, oga"
-  });
+
 });
