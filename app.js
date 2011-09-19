7e0f7ef43c8f609d743daf6f36c64d06ab65a5a7
Pedro Mateus Tavares <pedro@pedromtavares.com>
2011-09-19 19:32:11 -0300

    bulletproofing exceptions

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 /**
  * Module dependencies.
  */
@@ -170,3 +169,10 @@ if (app.settings.server.host){
   process.setuid(1000); 
   console.log("Process now running under user: " + process.getuid());
 }
+
+process.addListener('uncaughtException', function (err, stack) {
+  console.log('------------------------');
+  console.log('Exception: ' + err);
+  console.log(err.stack);
+  console.log('------------------------');
+});
