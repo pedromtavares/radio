ec520be415ff634ac202a9e9571dd83ec9867208
Pedro Mateus Tavares <pedro@pedromtavares.com>
2011-10-11 13:58:00 -0300

    possible bug fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -72,5 +72,5 @@ end
 before 'deploy', 'deploy:reset_shoutcast'
 before 'deploy:setup', 'deploy:create_deploy_to_with_sudo'
 after 'deploy:setup', 'deploy:write_upstart_script'
-after 'deploy', 'deploy:symlink_keys'
+before 'deploy', 'deploy:symlink_keys'
 after 'deploy', 'deploy:update_playlists'
