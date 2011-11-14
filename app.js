187f30dced6ec8e5c5415ed5899a977d637e1c4a
Pedro Mateus Tavares <pedro@pedromtavares.com>
2011-11-14 01:13:39 -0300

    this is it biatch

--- app.js
+++ app.js
@@ -8,6 +8,16 @@ require('./helpers/application_helper')(app);
 
 app.listen(app.settings.server.port, app.settings.server.host);
 
+setTimeout(function() {
+  // after reserving priviled port, set process to run on a less privileged user
+  if (app.settings.server.host){
+    process.setgid(50);
+    process.setuid(1000); 
+    console.log("Process now running under user: " + process.getuid());
+  }
+}, 5000)
+
+
 process.addListener('uncaughtException', function (err, stack) {
   console.log('------------------------');
   console.log('Exception: ' + err);
